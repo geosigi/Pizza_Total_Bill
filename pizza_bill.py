719b8e226c501ef52d788bf0f194a56a27a20fc6
최병곤 <127578263+geosigi@users.noreply.github.com>
2024-04-05 10:09:15 +0900

    pizza_bill.py

--- pizza_bill.py
+++ pizza_bill.py
@@ -1,1 +1,27 @@
-# 롤러코스터 타면서 사진도 찍을 것인가?print("롤로코스터에 오신 것을 환영합니다!")height = int(input("키가 몇 cm 입니까?"))bill=0if height >= 120:    print("롤러코스터를 타실 수 있습니다.")    age = int(input("나이가 몇살 입니까?"))    if age < 12:        bill =0        print("무료입니다!")    elif age <=18:        bill = 7000        print("청소년은 7천원입니다!")    else:        bill = 12000        print("성인은 만2천원입니다!")            wants_photo = input("사진을 찍기를 원하십니까? Yes(Y) or No(N).")    if wants_photo =="Y":        # 천원 추가        bill += 1000        print(f"지불하실 총액은 {bill}")        else:    print("죄송합니다. 롤러코스터를 탈 수 없습니다.")    print("파이썬 피자에 오신 것을 환영합니다!")pizza_size = input("원하시는 피자사이즈를 눌러 주세요! (S, M,L)")bill  = 0if pizza_size == "S" or pizza_size == "s":    bill = 15000    print(f"스몰은 {bill}원 입니다.")elif pizza_size == "M" or pizza_size == "m":    bill = 20000    print(f"미디움은 {bill}원 입니다.")elif pizza_size == "L" or pizza_size == "l":    bill = 30000    print(f"라지는 {bill}원 입니다.")else:    print("잘못 입력하셨습니다. 다시 실행해 주세요!")    wants_peperoni = input("페페로니를 추가는 2천원입니다. 추가하시겠습니까? 추가(Y) 아니오(N)")if wants_peperoni == "Y" or wants_peperoni == "y":    bill += 2000    print(f"페페로니를 추가한 금액은 {bill}원 입니다.")    wants_cheese = input("치즈 추가는 3천원입니다. 추가하시겠습니까? 추가(Y) 아니오(N)")if wants_cheese == "Y" or wants_cheese == "y":    bill += 3000    print(f"치즈를 추가한 금액은 {bill}원 입니다.")    print(f"총 지불하실 금액은 {bill}원 입니다.")        
+print("파이썬 피자에 오신 것을 환영합니다!")
+pizza_size = input("원하시는 피자사이즈를 눌러 주세요! (S, M,L)")
+bill  = 0
+
+if pizza_size == "S" or pizza_size == "s":
+    bill = 15000
+    print(f"스몰은 {bill}원 입니다.")
+elif pizza_size == "M" or pizza_size == "m":
+    bill = 20000
+    print(f"미디움은 {bill}원 입니다.")
+elif pizza_size == "L" or pizza_size == "l":
+    bill = 30000
+    print(f"라지는 {bill}원 입니다.")
+else:
+    print("잘못 입력하셨습니다. 다시 실행해 주세요!")
+    
+wants_peperoni = input("페페로니를 추가는 2천원입니다. 추가하시겠습니까? 추가(Y) 아니오(N)")
+if wants_peperoni == "Y" or wants_peperoni == "y":
+    bill += 2000
+    print(f"페페로니를 추가한 금액은 {bill}원 입니다.")
+    
+wants_cheese = input("치즈 추가는 3천원입니다. 추가하시겠습니까? 추가(Y) 아니오(N)")
+if wants_cheese == "Y" or wants_cheese == "y":
+    bill += 3000
+    print(f"치즈를 추가한 금액은 {bill}원 입니다.")
+    
+print(f"총 지불하실 금액은 {bill}원 입니다.")
